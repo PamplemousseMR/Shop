345098c6895b00396fd5a18f74d9251974a7ae90
romain <romain.manciaux@gmail.com>
2019-03-20 21:18:25 +0000

    refactor(basket): can be use without account

--- controller/basket.php
+++ controller/basket.php
@@ -5,33 +5,29 @@ require_once('php/IController.php');
 class Basket extends IController { 
     
     public static function router($request, $response, $args) { 
-        if(parent::isConnected()) {
-            $id = $_GET['item'];
-            global $entityManager;
-            $itemRepository = $entityManager->getRepository('Item');
-            $item = $itemRepository->findOneBy(array('id'=>$id));
-            if($item != null) {
-                self::addToCart($item);
-            }
-            $list = self::getCart();
-            $items = array();
-            $quantities = array();
-            $total = 0;
-            if($list != null) {
-                foreach($list as $id => $quantity) {
-                    $item = $itemRepository->findOneBy(array('id'=>$id));
-                    if($item == null) {
-                        return $response->withStatus(302)->withHeader('Location', '/home');
-                    }
-                    array_push($items, $item);
-                    array_push($quantities, $quantity);
-                    $total += $quantity * $item->getPrice();
-                }
-            }
-            parent::render($response, 'basket.twig', array('items' => $items, 'quantities' => $quantities, 'total' => $total));
-        } else {
-            return $response->withStatus(302)->withHeader('Location', '/home');
+        $id = $_GET['item'];
+        global $entityManager;
+        $itemRepository = $entityManager->getRepository('Item');
+        $item = $itemRepository->findOneBy(array('id'=>$id));
+        if($item != null) {
+            self::addToCart($item);
         }
+        $list = self::getCart();
+        $items = array();
+        $quantities = array();
+        $total = 0;
+        if($list != null) {
+            foreach($list as $id => $quantity) {
+                $item = $itemRepository->findOneBy(array('id'=>$id));
+                if($item == null) {
+                    return $response->withStatus(302)->withHeader('Location', '/home');
+                }
+                array_push($items, $item);
+                array_push($quantities, $quantity);
+                $total += $quantity * $item->getPrice();
+            }
+        }
+        parent::render($response, 'basket.twig', array('items' => $items, 'quantities' => $quantities, 'total' => $total));
     }
 
     public static function addToCart($item) {
